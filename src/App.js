525590f4ee181ea8cf0d2700b40af6ebcfe7d479
91d906h4 <fairymanning@gmail.com>
2022-09-19 02:53:36 +0900

    fix responsive image map

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
-import { useCookies } from "react-cookie";
-import { Helmet } from "react-helmet";
 import CityStation from "./components/CityStation";
 
 function App() {
-    const [ cookies, setCookies ] = useCookies();
-
     return (
         <CityStation />
     );
